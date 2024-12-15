bd08f61da155b07da8c1983888719139fd2e52fd
Ejay07 <edricjustin007@gmail.com>
2024-12-15 13:26:26 +0700

    Making the template

---
+++ main.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+class ToDoList(tk.Window): 
+    def __init__(self): 
+        super().__init__()
+
+# functions 
+
+
+
+if __name__ == "__main__": 
+    ToDoList().mainloop()
