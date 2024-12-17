c7734760d9007718d3828b638af24329d6c8a33b
Ejay07 <edricjustin007@gmail.com>
2024-12-17 21:14:29 +0700

    making the dimensions

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import tkinter as tk
 
-class ToDoList(tk.Window): 
-    def __init__(self): 
-        super().__init__()
+class ToDoList: 
+    def __init__(self, root): 
+        self.root = root
+        self.root.title("To Do List") 
+        self.root.geometry("700x700") 
+        self.root.resizable(False, False)
 
 # functions 
 
 
 
 if __name__ == "__main__": 
-    ToDoList().mainloop()
+    root = tk.Tk()
+    app = ToDoList(root)
+    app.root.mainloop()
