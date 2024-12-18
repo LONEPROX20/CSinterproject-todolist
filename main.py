51f2dcdd79e93739d86dff70d9424f07a70e1e6b
Ejay07 <edricjustin007@gmail.com>
2024-12-18 14:30:32 +0700

    task inputs and listbox

--- main.py
+++ main.py
@@ -12,11 +12,18 @@ class ToDoList:
         # buttons appearance dlu
         self.add_task_icon = tk.PhotoImage(file='add_task.png')
         self.add_task_button = tk.Button(root, image=self.add_task_icon, command=self.add_task)
-        self.add_task_button.pack(padx=100,pady=100)
+        self.add_task_button.pack(pady=55)
 
         self.remove_task_icon = tk.PhotoImage(file='remove_tasl.png')
         self.remove_task_button = tk.Button(root, image=self.remove_task_icon,command=self.remove_task)
-        self.remove_task_button.pack(padx=100, pady=10)
+        self.remove_task_button.pack(pady=15)
+
+        self_task_list = []
+        self.task_inputs = tk.Entry(root, width=50)
+        self.task_inputs.pack(pady=15)
+
+        self.task_listbox = tk.Listbox(root, width=55, height=20)
+        self.task_listbox.pack(pady=40)
 
     def add_task(self):
         pass
